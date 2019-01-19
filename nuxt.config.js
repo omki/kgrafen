6c5b8e8f4c5402643080b41b22fb37475eb7101c
omki <aagaardmikkeli@gmail.com>
2019-01-19 17:38:58 +0100

    google meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { name: 'google-site-verification', content: 'zd43zGcgKeV2nOz2J44QnWCx1QYbO75peKPSpGhtlhY' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
