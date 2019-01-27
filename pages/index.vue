84edf6c85fef9518c496121270852722d4d6e4ee
omki <aagaardmikkeli@gmail.com>
2019-01-27 15:21:50 +0100

    image on home

--- pages/index.vue
+++ pages/index.vue
@@ -3,34 +3,45 @@
     <div class="page-container">
       <h1>Polish Science Committee in charge by APEE for Graphene Technology</h1>
 
-      Program assumptions
-Issues of the SEP Committee on graphene technology - KS Grafen fully complies with the implementation of the statutory activity of SEP based mainly on the six pillars of activity:
+      <div class="float-full">
+        <div class="float-half-left">
+          <img src="../assets/images/KSGrafenSymbolTrans50.png">
+          Polski Komitet Naukowy SEP do spraw Technologii Grafenowej ~ filary działalności
+        </div>
 
-Activities
-Mobilizing
-Archiving
-Research
-Commercial
-Scientific
-Popularization
-Ad.1. Mobilizing Activity
-Activating activities are primarily based on the work of the SEP Committee for Technology, (symbol) KS Grafen with additional thematic groups.
+        <div class="float-half-right">
 
-Ad.2. Archiving Activity
-The archiving activity is based on the creation of databases, literature, patent databases, reports, reviews, summaries of research results.
+          Program assumptions
+          Issues of the SEP Committee on graphene technology - KS Grafen fully complies with the implementation of the statutory activity of SEP based mainly on the six pillars of activity:
 
-Ad.3. Research Activity
-The research activity is based on the active involvement of the Committee in the consortia formed with the Scientific and Research Units consisting in sharing their own source materials.
+          Activities
+          Mobilizing
+          Archiving
+          Research
+          Commercial
+          Scientific
+          Popularization
+          Ad.1. Mobilizing Activity
+          Activating activities are primarily based on the work of the SEP Committee for Technology, (symbol) KS Grafen with additional thematic groups.
 
-Ad.4. Commercial Activity
-The commercialization activity is aimed at creating a structure aiming at commercialization of scientific and research projects based on cooperation with SMEs, OBR, institutes in the consortium with the aim of selecting within the committee its own structure allowing for commercial projects (eg grants) within the SEP Committee to graphene technology.
+          Ad.2. Archiving Activity
+          The archiving activity is based on the creation of databases, literature, patent databases, reports, reviews, summaries of research results.
 
-Ad.5. Scientific Activity
-Scientific activity is to carry out systematic and reviewing works and their publications.
+          Ad.3. Research Activity
+          The research activity is based on the active involvement of the Committee in the consortia formed with the Scientific and Research Units consisting in sharing their own source materials.
 
-Ad.6. Popularization Activity
-Promotional activity is aimed at developing Polish and foreign publications, books, contributing to the technical press and the SEP newsletter, creating expert and advisory work, running advertising campaigns, and supporting the literature in the thematic scope of KS Grafen.
+          Ad.4. Commercial Activity
+          The commercialization activity is aimed at creating a structure aiming at commercialization of scientific and research projects based on cooperation with SMEs, OBR, institutes in the consortium with the aim of selecting within the committee its own structure allowing for commercial projects (eg grants) within the SEP Committee to graphene technology.
+
+          Ad.5. Scientific Activity
+          Scientific activity is to carry out systematic and reviewing works and their publications.
+
+          Ad.6. Popularization Activity
+          Promotional activity is aimed at developing Polish and foreign publications, books, contributing to the technical press and the SEP newsletter, creating expert and advisory work, running advertising campaigns, and supporting the literature in the thematic scope of KS Grafen.
+        </div>
+      </div>
     </div>
+
   </section>
 </template>
 
