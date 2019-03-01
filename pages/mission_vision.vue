622d0315f42f3c320f4898111b2fdab4205c7d3f
omki <aagaardmikkeli@gmail.com>
2019-03-01 13:16:48 +0100

    mision and vision update

--- pages/mission_vision.vue
+++ pages/mission_vision.vue
@@ -2,6 +2,30 @@
   <section id="page">
     <div class="page-container">
       <h1>{{$t('page.header.title')}} ~ {{$t('navigation.mission_vision')}}</h1>
+      <div class="float-full">
+        <div class="float-half-left">
+          <div class="float-half-left">
+            <iframe 
+              src="https://sep.com.pl/tydzienSEP/2017/tydzien_w_SEP_147.pdf" 
+              height="500"
+              frameborder="0">
+              </iframe>
+          </div>
+          <div class="float-half-right">
+            <iframe 
+              src="https://graphenequantec.files.wordpress.com/2019/03/zebranie-inauguracyjne-rady-naukowo-technicznej-sep-rnt-sep.pdf" 
+              height="500"
+              frameborder="0">
+              </iframe>
+          </div>
+        </div>
+        <div class="float-half-right">
+          <h2>{{$t('pages.mision_vision.mission.title')}}</h2>
+          <p>{{$t('pages.mision_vision.mission.content')}}</p>
+          <h2>{{$t('pages.mision_vision.vision.title')}}</h2>
+          <p>{{$t('pages.mision_vision.vision.content')}}</p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -13,6 +37,9 @@ export default {
 </script>
 
 <style>
+h2 {
+  margin-top: 20px;
+}
 .page-container {
   height: 2000px;
 }
