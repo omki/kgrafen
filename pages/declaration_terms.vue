4fb6a4a8c932579194b9c24683ef4e0eedb4cd33
omki <aagaardmikkeli@gmail.com>
2019-03-06 23:11:36 +0100

    deklaracja

--- pages/declaration_terms.vue
+++ pages/declaration_terms.vue
@@ -2,6 +2,28 @@
   <section id="page">
     <div class="page-container">
       <h1>{{$t('page.header.title')}} ~ {{$t('navigation.declaration_terms')}}</h1>
+
+      <h2>Deklaracja</h2>
+      <h2>Uchwała</h2>
+      <div class="column-3">
+        <img src="../assets/images/declaration/uchwala.jpg" alt="">
+      </div>
+      <h2>Regulamin</h2>
+      <div class="column-3">
+        <img src="../assets/images/declaration/regulamin1.jpg" alt="">
+      </div>
+      <div class="column-3">
+        <img src="../assets/images/declaration/regulamin2.jpg" alt="">
+      </div>
+      <div class="column-3">
+        <img src="../assets/images/declaration/regulamin3.jpg" alt="">
+      </div>
+      <h2>Statut</h2>
+      <div class="column-3">
+        <a href="https://graphenequantec.files.wordpress.com/2019/03/statutsep.pdf" target="_blank">
+          <img src="../assets/images/declaration/statut.jpg" alt="">
+        </a>
+      </div>
     </div>
   </section>
 </template>
@@ -13,11 +35,19 @@ export default {
 </script>
 
 <style>
+h2{
+  margin-top: 20px;
+}
 .page-container {
   height: 2000px;
 }
 
 .links {
   padding-top: 15px;
+}
+
+.column-3 {
+  display: inline-grid;
+  width: 30%;
 }
 </style>
