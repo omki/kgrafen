e9b66a40c1fdfcb12e765775068fcd3c397b984a
omki <aagaardmikkeli@gmail.com>
2019-03-06 22:02:24 +0100

    facebook posts added

--- pages/social.vue
+++ pages/social.vue
@@ -19,9 +19,68 @@
         </div>
       </div>
 
+      <div class="column-3" fxFlex="30">
+        <div class="socialimage">
+          <img src="../assets/images/social/twitter.png" alt="">
+        </div>
+        <no-ssr>
+          <div class="fb-page" data-href="https://www.facebook.com/KGrafen/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/KGrafen/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/KGrafen/">Polski Komitet Naukowy SEP ds Technologii grafenowej</a></blockquote></div>
+        </no-ssr>
+      </div>
+
     </div>
   </section>
 </template>
+
+<script>
+// import { FB } from '../plugins/fb-sdk.js'
+
+export default {
+    //   data: function () {
+    //     return {isFBReady: false}
+    // },
+    // mounted: function () {
+    //     this.isFBReady = Vue.FB != undefined
+    //     window.addEventListener('fb-sdk-ready', this.onFBReady)
+    // },
+    // beforeDestroy: function () {
+    //     window.removeEventListener('fb-sdk-ready', this.onFBReady)
+    // },
+    // methods: {
+    //     onFBReady: function () {
+    //       this.isFBReady = true
+    //     }
+    // }
+
+  created: function() {
+    if (process.client) {
+    console.log('created main');
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '208927772784560',
+        xfbml      : true,
+        version    : 'v2.7'
+      });
+
+      //This function should be here, inside window.fbAsyncInit
+    //   FB.getLoginStatus(function(response) {
+    //     console.log(response);
+    //  });
+
+   };
+
+    (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+  }
+}
+
+</script>
 
 <style>
 .page-container {
@@ -33,6 +92,7 @@
 }
 
 .column-3 {
+  display: inline-grid;
   margin-top: 30px;
   text-align: center;
   width: 33.33%;
