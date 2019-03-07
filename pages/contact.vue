0f4343e269133b885e1af343bcf584059e0f6114
omki <aagaardmikkeli@gmail.com>
2019-03-07 21:38:48 +0100

    access form

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,6 +4,17 @@
       <h1>{{$t('page.header.title')}} ~ {{$t('navigation.contact')}}</h1>
       <br/>
       {{$t('pages.contact.send_email')}} <a href="mailto:komitetgrafen@gmail.com">komitetgrafen@gmail.com</a>
+
+      <div class="accessForm">
+        <h2>Proszę o przyjęcie mnie w poczet członków komitetu</h2>
+        <div class="name">Imię i Nazwisko <input type="text"></div>
+        <div class="email">Email <input type="text"></div>
+        <div class="telephone">Telefon <input type="text"></div>
+        <div class="terms"><input type="checkbox"> Zapoznałe(a)m się z <nuxt-link v-bind:to="'/declaration_terms'">Regulaminem</nuxt-link> Polskiego Komitetu SEP ds. Technologii Grafenowej.</div>
+        <div class="telephone"><button type="button">Wyślij</button></div>
+      </div>
+
+      Dziękuję za wypełnienie wormularza P. O. Przewodniczącego Komitetu dr Tadeusz Habdank Wojewódzki
     </div>
   </section>
 </template>
@@ -15,5 +26,9 @@
 
 .links {
   padding-top: 15px;
+}
+
+.accessForm > div {
+  margin: 5px 0;
 }
 </style>
