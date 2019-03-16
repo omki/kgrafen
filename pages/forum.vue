42f5ea292cf82e4c0bd6ddbe8b86286af2ffbced
omki <aagaardmikkeli@gmail.com>
2019-03-16 14:38:32 +0100

    video conference room at forum

--- pages/forum.vue
+++ pages/forum.vue
@@ -3,6 +3,14 @@
     <div class="page-container">
       <h1>{{$t('page.header.title')}} ~ {{$t('navigation.forum')}}</h1>
       
+      <div class="join_conference">
+        <h3>{{$t('pages.contact.join_video_conference_room')}}</h3><br/>
+        <a href="http://appear.in/grafen" target="_blank">
+          <img src="../assets/images/videoconferenceroom.png" alt="">
+        </a>
+      </div>
+      
+      <br/><br/>
     </div>
   </section>
 </template>
@@ -20,5 +28,11 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+.join_conference {
+  text-align: center;
+  width: 50%;
+  margin: 0 100px;
 }
 </style>
